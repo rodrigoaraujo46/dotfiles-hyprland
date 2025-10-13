feebd7f29a9610d004d484f215d786c6106b419b
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2025-10-13 09:08:33 +0100

    Telescope: add option to look for hidden files.

--- .config/nvim/lua/animus/lazy/telescope.lua
+++ .config/nvim/lua/animus/lazy/telescope.lua
@@ -18,6 +18,9 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ph', function()
+            builtin.find_files({ hidden = true, no_ignore = true })
+        end)
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
